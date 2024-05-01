fad813f6a831fbea4d0edcb067312830efa304be
Davi Borges Sistemas <daviborges.sistemas@gmail.com>
2024-05-01 17:39:22 -0300

    splash

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,7 +1,6 @@
+import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
-
-import '../../core/config/env/env.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -9,20 +8,25 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
-      ),
-      body: Column(
-        children: [
-          Container(),
-          DeliveryButton(
-            onPressed: () {},
-            label: Env.i['backend_base_url'] ?? '',
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'text'),
-          ),
-        ],
+      body: ColoredBox(
+        color: const Color(0XFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(children: [Image.asset('assets/images/logo.png')]),
+            )
+          ],
+        ),
       ),
     );
   }
